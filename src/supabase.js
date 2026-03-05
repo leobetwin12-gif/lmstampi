ba90fc0d68f39a1354c8554beec3e45fbb72ce2f
leobetwin12-gif <leobetwin12@gmail.com>
2026-03-05 21:45:59 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -5,7 +5,7 @@
 // ─────────────────────────────────────────────────────────────────────────────
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL     = 'https://rcuktraoqncshrwvgrmy.supabase.co'   // ← incolla qui la tua URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjdWt0cmFvcW5jc2hyd3Zncm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzY0NzMsImV4cCI6MjA4ODE1MjQ3M30.yStMv4korNn5xAOJDKUjmFI5JGZ1t52ACudcTWGGZUw' // ← incolla qui la tua anon key
+const SUPABASE_URL     = 'https://ltesmompnluohtsolrfu.supabase.co'   // ← incolla qui la tua URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0ZXNtb21wbmx1b2h0c29scmZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NDEwMDksImV4cCI6MjA4ODMxNzAwOX0.Sl9toZf1--sepvm6q6Z9jmAZktbu8TWsM0eZrGKqZIU' // ← incolla qui la tua anon key
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
